0bb7edd9e02cc768f7b34cac998c7ecc7e24ffb7
Alireza Inanlou <alireza.9900@gmail.com>
2025-12-02 23:51:23 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+# service-worker.js
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('coffee-pwa-v1').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json'
+    ]))
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
