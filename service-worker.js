ece9170f65b3622b5d20ed1a02b533f8f8d4c13c
Alireza Inanlou <alireza.9900@gmail.com>
2025-12-06 14:33:39 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,31 @@
+const CACHE_NAME = 'coffee-pwa-v1.1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('coffee-pwa-v1').then(cache => cache.addAll([
-      './',
-      './index.html',
-      './manifest.json'
-    ]))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
   );
 });
